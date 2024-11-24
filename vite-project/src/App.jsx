54b8d0c2a65be72895aafa302fbc1d2824ab0144
Seyidova Minure <minure.seyidova@gmail.com>
2024-11-24 15:37:06 +0400

    user login is ready

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import UserLogin from "./components/UserLogin";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <UserLogin />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/UserLogin.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import eyeIcon from "../assets/svg/eyeIcon.svg";
+class UserLogin extends Component {
+  handleEmailChange = () => {
+    console.log("Yalnız .ru domenlərinə icazə verilir.");
+  };
+  handlePasswordChange = () => {
+    console.log("Ən azı 8 simvol");
+  };
+  render() {
+    return (
+      <div className="login">
+        <div className="email">
+          <input
+            onChange={this.handleEmailChange}
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="password">
+          <input
+            onChange={this.handlePasswordChange}
+            type="password"
+            placeholder="Password"
+          />
+          <img className="icon" src={eyeIcon} alt="" />
+        </div>
+        <button className="login-button">Login</button>
+      </div>
+    );
+  }
+}
+
+export default UserLogin;
